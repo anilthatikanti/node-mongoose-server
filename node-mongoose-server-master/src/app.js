588bcd075af583743653a749cb4f17b10c7e822b
anil thatikanti <anil.thatikanti@tandemloop.in>
2025-06-14 22:24:09 +0530

    cors policy added

--- node-mongoose-server-master/src/app.js
+++ node-mongoose-server-master/src/app.js
@@ -26,7 +26,7 @@ subscriptionService.initialize().catch(error => {
 console.log('Subscription service initialized');
 
 // 🟢 Apply CORS FIRST so it doesn't get blocked by Helmet
-const allowedOrigins = ['https://angular-stock.netlify.app', 'http://localhost:4200'];
+const allowedOrigins = ['https://angular-stock.netlify.app','https://finsync-hazel.vercel.app', 'http://localhost:4200'];
 
 app.use(cors({
   origin: function (origin, callback) {
